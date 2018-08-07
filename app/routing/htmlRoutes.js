9aa6080dba9e43b5d038c5e29c84aebce0d561dd
Guillermo Barila <gui365@yahoo.com.ar>
2018-08-07 15:52:17 -0400

    create survey

--- app/routing/htmlRoutes.js
+++ app/routing/htmlRoutes.js
@@ -1,2 +1,12 @@
 // A GET Route to /survey which should display the survey page.
 // A default, catch-all route that leads to home.html which displays the home page.
+
+// Routing
+app.get("/", (request, response) => {
+  // app.sendFile(express.static("public"));
+  response.sendFile(path.join(__dirname, "app/public/home.html"));
+});
+
+app.get("/survey", (request, response) => {
+  response.sendFile(path.join(__dirname, "app/public/survey.html"));
+});
--- server.js
+++ server.js
@@ -4,8 +4,30 @@ var path = require("path");
 
 // Set up express
 var app = express();
-var port = 3000;
+var PORT = process.env.PORT || 3000;
 
 // Set up bodyParser
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
+
+// Set up server to listen on port
+app.listen(PORT, () => {
+  console.log(`Listening on port ${PORT}`);
+});
+
+app.use(express.static('app/public'));
+
+
+// A GET Route to /survey which should display the survey page.
+// A default, catch-all route that leads to home.html which displays the home page.
+
+// Routing
+// app.get("/", (request, response) => {
+//   // app.sendFile(express.static("public"));
+//   response.sendFile(path.join(__dirname, "app/public/home.html"));
+//   console.log(__dirname);
+// });
+
+// app.get("/survey", (request, response) => {
+//   response.sendFile(path.join(__dirname, "app/public/survey.html"));
+// });
