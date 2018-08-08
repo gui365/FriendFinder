f0aafe28515a0f936921bdf63ee1cafe631f7b81
Guillermo Barila <gui365@yahoo.com.ar>
2018-08-08 13:54:51 -0400

    Successfully created html routes and linked to server.js

--- app/routing/htmlRoutes.js
+++ app/routing/htmlRoutes.js
@@ -1,12 +1,19 @@
 // A GET Route to /survey which should display the survey page.
 // A default, catch-all route that leads to home.html which displays the home page.
+var path = require("path");
 
 // Routing
-app.get("/", (request, response) => {
+var home = (request, response) => {
   // app.sendFile(express.static("public"));
-  response.sendFile(path.join(__dirname, "app/public/home.html"));
-});
+  response.sendFile(path.join(__dirname, "../public/home.html"));
+};
 
-app.get("/survey", (request, response) => {
-  response.sendFile(path.join(__dirname, "app/public/survey.html"));
-});
+var survey = (request, response) => {
+  // app.sendFile(express.static("public"));
+  response.sendFile(path.join(__dirname, "../public/survey.html"));
+};
+
+module.exports.htmlRoutes = {
+  home: home,
+  survey: survey
+}
--- server.js
+++ server.js
@@ -1,33 +1,19 @@
-var express = require("express");
-var bodyParser = require("body-parser");
-var path = require("path");
+var express = require("express"),
+    bodyParser = require("body-parser"),
+    htmlRoutes = require("./app/routing/htmlRoutes");
 
 // Set up express
 var app = express();
 var PORT = process.env.PORT || 3000;
-
-// Set up bodyParser
-app.use(bodyParser.urlencoded({ extended: true }));
-app.use(bodyParser.json());
-
 // Set up server to listen on port
 app.listen(PORT, () => {
   console.log(`Listening on port ${PORT}`);
 });
 
-app.use(express.static('app/public'));
-
-
-// A GET Route to /survey which should display the survey page.
-// A default, catch-all route that leads to home.html which displays the home page.
+// Set up bodyParser
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
 
 // Routing
-// app.get("/", (request, response) => {
-//   // app.sendFile(express.static("public"));
-//   response.sendFile(path.join(__dirname, "app/public/home.html"));
-//   console.log(__dirname);
-// });
-
-// app.get("/survey", (request, response) => {
-//   response.sendFile(path.join(__dirname, "app/public/survey.html"));
-// });
+app.get("/", htmlRoutes.htmlRoutes.home);
+app.get("/survey", htmlRoutes.htmlRoutes.survey);
